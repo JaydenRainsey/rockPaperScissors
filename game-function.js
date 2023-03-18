279c885f00221849532e62a69d01a6b6371c15d0
jaydenrainsey <jaydenrainsey@gmail.com>
2023-03-18 17:43:58 -0500

    added welcoming message prompt

---
+++ game-function.js
@@ -0,0 +1,5 @@
+// User enters name with prompt, stored in a variable
+let userName = prompt("Enter your name: ");
+
+// Display welcome message
+alert("Welcome to Rock-Paper-Scissors, " + userName);
