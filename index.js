7b5d1df331d9d2a3039249a219cf53a34332b54b
jaydenrainsey <jaydenrainsey@gmail.com>
2023-03-17 22:31:10 -0500

    outlined problem, and a plan as comments

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 // Understand the Problem
-
-
+/* 
+This is a game of rock/paper/scissors played from the console.
+The game is played against the computer.
+*/
 // Plan
+/* 
+1. Write skeleton html code and push to github.
+2. Inputs will be the user's choice from an alert prompt.
+3. The output will be to reveal player's choice and the cpu's choice,
+also decide the winner. The logic that compares the choices will be a series of conditional statements.
+4. The computer's choice will be random, which will require the random math function.
 
-
+*/
 // Psuedocode
 
 
