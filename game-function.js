681d6388486e467d3662ad2e68be4a1a8e5ba6f2
jaydenrainsey <jaydenrainsey@gmail.com>
2023-03-18 20:48:42 -0500

    added random function

--- game-function.js
+++ game-function.js
@@ -3,3 +3,8 @@ let userName = prompt("Enter your name: ");
 
 // Display welcome message
 alert("Welcome to Rock-Paper-Scissors, " + userName);
+
+// Computer's choice from an array
+let cpuArray = ["ROCK", "PAPER", "SCISSORS"]
+let randomChoice = cpuArray[Math.floor(Math.random() * cpuArray.length)];
+alert(randomChoice);
